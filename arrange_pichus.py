dca9d269204db9f0b5714e6d0929b49e89eae93d
radhe <radverma@iu.edu>
2021-02-07 23:36:33 -0500

    Working Part 2 with long time

--- arrange_pichus.py
+++ arrange_pichus.py
@@ -9,7 +9,7 @@
 
 
 import sys
-from queue import PriorityQueue
+import time
 
 # Parse the map from a given filename
 def parse_map(filename):
@@ -29,12 +29,31 @@ def add_pichu(board, row, col):
     return board[0:row] + [board[row][0:col] + ['p',] + board[row][col+1:]] + board[row+1:]
 
 def is_pichus_hidden_on_board(board, row, col):
-    try:
-        return is_pichus_hidden_in_line([row[col] for row in board], row) and is_pichus_hidden_in_line(board[row][:], col)
-    except Exception as e:
-        print(str(row) + ", " + str(col))
-        raise e
+    return is_pichus_hidden_in_line([row[col] for row in board], row) and is_pichus_hidden_in_line(board[row][:], col)
 
+def is_pichus_hidden_on_board2(board, row, col):
+    hidden = is_pichus_hidden_in_line([row[col] for row in board], row) and is_pichus_hidden_in_line(board[row][:], col)
+    if not hidden:
+        return False
+    diagonals = get_diagonals_with_pivot(board, row, col)
+
+    return is_pichus_hidden_in_line(diagonals[0][0], diagonals[0][1]) and is_pichus_hidden_in_line(diagonals[1][0], diagonals[1][1])
+
+def get_diagonals_with_pivot(board, row, col):
+    line1 = []
+    line2 = []
+    for i in range(len(board)):
+        col1 = col + (i - row)
+        col2 = col + (row - i)
+        if (i == row):
+            pivot1 = len(line1)
+            pivot2 = len(line2)
+        if (col1 >= 0 and col1 < len(board[0])):
+            line1.append(board[i][col1])
+        if (col2 >= 0 and col2 < len(board[0])):
+            line2.append(board[i][col2])
+
+    return [(line1, pivot1), (line2, pivot2)]
 
 def is_pichus_hidden_in_line(line, index):
     i = index - 1
@@ -60,10 +79,13 @@ def is_pichus_hidden_in_line(line, index):
 def successors(board):
     return [ add_pichu(board, r, c) for r in range(0, len(board)) for c in range(0,len(board[0])) if board[r][c] == '.' and is_pichus_hidden_on_board(board, r, c) ]
 
+# get successors for diagonally impacted agents
+def successors_diagonal(board):
+    return [ add_pichu(board, r, c) for r in range(0, len(board)) for c in range(0,len(board[0])) if board[r][c] == '.' and is_pichus_hidden_on_board2(board, r, c) ]
+
 # check if board is a goal state
 def is_goal(board, k):
     return count_pichus(board) == k 
-
 
 # Arrange agents on the map
 #
@@ -73,17 +95,33 @@ def is_goal(board, k):
 # - success is True if a solution was found, and False otherwise.
 #
 def solve(initial_board, k):
-    count = 1
+    if k == 0:
+        return solve_diagonal(initial_board)
+
     fringe = [(initial_board, count_pichus(initial_board))]
     while len(fringe) > 0:
         (board, pichus_count) = fringe.pop()
         for s in successors( board ):
-            count = count + 1
             if pichus_count + 1 == k:
-                print(count)
                 return(s,True)
             fringe.append((s, pichus_count + 1))
+            # fringe = [(s, pichus_count + 1)] + fringe
     return ([],False)
+
+def solve_diagonal(initial_board):
+    max_pichus = count_pichus(initial_board)
+    max_pichus_board = initial_board
+
+    fringe = [(initial_board, count_pichus(initial_board))]
+
+    while len(fringe) > 0:
+        (board, pichus_count) = fringe.pop()
+        for s in successors_diagonal(board):
+            if pichus_count + 1 > max_pichus:
+                max_pichus_board = s
+                max_pichus = pichus_count + 1
+            fringe.append((s, pichus_count + 1))
+    return (max_pichus_board,True)
 
 # Main Function
 if __name__ == "__main__":
@@ -92,8 +130,9 @@ if __name__ == "__main__":
     # This is K, the number of agents
     k = int(sys.argv[2])
     print ("Starting from initial board:\n" + printable_board(house_map) + "\n\nLooking for solution...\n")
+
+    # start = time.process_time()
     (newboard, success) = solve(house_map, k)
+    # print(time.process_time() - start)
     print ("Here's what we found:")
     print (printable_board(newboard) if success else "None")
-
-
