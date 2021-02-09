81c878ada1a1e0cb9b69c0165ac23d128b5059fe
radhe <radverma@iu.edu>
2021-02-09 02:58:04 -0500

    Added data structures for storing states

--- arrange_pichus.py
+++ arrange_pichus.py
@@ -10,6 +10,11 @@
 
 import sys
 import time
+#
+# class StateObject:
+#     col, row = -1
+#     board = [[]]
+
 
 # Parse the map from a given filename
 def parse_map(filename):
@@ -87,6 +92,113 @@ def successors_diagonal(board):
 def is_goal(board, k):
     return count_pichus(board) == k 
 
+from queue import SimpleQueue, PriorityQueue, LifoQueue
+
+
+class StateObject:
+    def __init__(self, object, priority):
+        self.data = object
+        self.priority = priority
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+def get_fringe(algo):
+    if algo in ("list_stack", "list_queue"):
+        return []
+    if algo in ("pq_dfs"):
+        return PriorityQueue()
+    if algo == "queue":
+        return SimpleQueue()
+    if algo == "lifo_q_stack":
+        return LifoQueue()
+
+def insert_in_fringe(fringe, item, algo):
+    if algo == "list_stack":
+        fringe.append(item)
+    if algo == "queue":
+        fringe.put(item)
+    if algo == "lifo_q_stack":
+        fringe.put(item)
+    if algo == "pq_dfs":
+        if item[1] == 0:
+            priority = 0
+        else:
+            priority = 1 / item[1]
+        fringe.put((StateObject(item, priority)))
+
+def get_fringe_item(fringe, algo):
+    if algo in ("list_stack", "list_queue"):
+        return fringe.pop()
+    if algo in ("pq_dfs"):
+        return fringe.get().data
+    if algo in ("queue", "lifo_q_stack"):
+        return fringe.get()
+
+def is_fringe_empty(fringe, algo):
+    if algo in ("list_stack", "list_queue"):
+        return len(fringe) == 0
+    if algo in ("queue", "pq_dfs", "lifo_q_stack"):
+        return fringe.empty()
+
+def solve_diagonal(initial_board):
+
+    max_pichus = count_pichus(initial_board)
+    max_pichus_board = initial_board
+
+    state_count = 1
+    max_fringe_size = 1
+    fringe_size = 1
+    total_insert_time = 0
+    algo_start_time = time.process_time()
+
+    algo = "list_queue"
+
+    fringe = get_fringe(algo)
+
+    if algo != "list_queue":
+        insert_in_fringe(fringe, (initial_board, count_pichus(initial_board)), algo)
+    else:
+        fringe = [(initial_board, count_pichus(initial_board))]
+
+    while not is_fringe_empty(fringe, algo):
+
+        (board, pichus_count) = get_fringe_item(fringe, algo)
+        fringe_size = fringe_size - 1
+
+        for s in successors_diagonal(board):
+            state_count = state_count + 1
+            fringe_size = fringe_size + 1
+            if fringe_size > max_fringe_size:
+                max_fringe_size = fringe_size
+
+            if pichus_count + 1 > max_pichus:
+                max_pichus_board = s
+                max_pichus = pichus_count + 1
+
+            item = (s, pichus_count + 1)
+
+            start_time = time.process_time()
+            if algo != "list_queue":
+                insert_in_fringe(fringe, item, algo)
+            else:
+                fringe = [item] + fringe
+            total_insert_time = total_insert_time + (time.process_time() - start_time)
+
+            if state_count % 1000 == 0:
+                print("Avg time: " + str(total_insert_time / state_count))
+                print("States count: " + str(state_count))
+                print("max fringe size: " + str(max_fringe_size))
+                print("pichus_count: " + str(pichus_count + 1))
+
+    print("Algo: " + algo)
+    print("States explored: " + str(state_count))
+    print("Max Fringe size: " + str(max_fringe_size))
+    print("total time in insert ops: " + str(total_insert_time))
+    print("avg time in fringe inserts: " + str(total_insert_time / state_count))
+    print("Total time taken by algo: " + str(time.process_time() - algo_start_time))
+
+    return (max_pichus_board,True)
+
 # Arrange agents on the map
 #
 # This function MUST take two parameters as input -- the house map and the value k --
@@ -98,30 +210,65 @@ def solve(initial_board, k):
     if k == 0:
         return solve_diagonal(initial_board)
 
-    fringe = [(initial_board, count_pichus(initial_board))]
-    while len(fringe) > 0:
-        (board, pichus_count) = fringe.pop()
-        for s in successors( board ):
+    state_count = 1
+    max_fringe_size = 1
+    fringe_size = 1
+    total_insert_time = 0
+    algo_start_time = time.process_time()
+
+    algo = "list_stack"
+    fringe = get_fringe(algo)
+
+    solution = None
+
+    if algo != "list_queue":
+        insert_in_fringe(fringe, (initial_board, count_pichus(initial_board)), algo)
+    else:
+        fringe = [(initial_board, count_pichus(initial_board))]
+
+    while not is_fringe_empty(fringe, algo):
+        (board, pichus_count) = get_fringe_item(fringe, algo)
+        fringe_size = fringe_size - 1
+
+        for s in successors_diagonal( board ):
+            state_count = state_count + 1
             if pichus_count + 1 == k:
-                return(s,True)
-            fringe.append((s, pichus_count + 1))
-            # fringe = [(s, pichus_count + 1)] + fringe
+                solution = (s,True)
+                break
+
+            fringe_size = fringe_size + 1
+            if max_fringe_size < fringe_size:
+                max_fringe_size = fringe_size
+
+            start_time = time.process_time()
+
+            if algo != "list_queue":
+                insert_in_fringe(fringe, ((s, pichus_count + 1)), algo)
+            else:
+                fringe = [(s, pichus_count + 1)] + fringe
+
+            if state_count % 1000 == 0:
+                print("Avg time: " + str(total_insert_time / state_count))
+                print("States count: " + str(state_count))
+                print("max fringe size: " + str(max_fringe_size))
+                print("pichus_count: " + str(pichus_count + 1))
+
+            total_insert_time = total_insert_time + (time.process_time() - start_time)
+        if solution is not None:
+            break
+
+    print("Algo: " + algo)
+    print("States explored: " + str(state_count))
+    print("Max Fringe size: " + str(max_fringe_size))
+    print("total time in insert ops: " + str(total_insert_time))
+    print("avg time in fringe inserts: " + str(total_insert_time/state_count))
+    print("Total time taken by algo: " + str(time.process_time() - algo_start_time))
+
+    if solution is not None:
+        return solution
+
     return ([],False)
 
-def solve_diagonal(initial_board):
-    max_pichus = count_pichus(initial_board)
-    max_pichus_board = initial_board
-
-    fringe = [(initial_board, count_pichus(initial_board))]
-
-    while len(fringe) > 0:
-        (board, pichus_count) = fringe.pop()
-        for s in successors_diagonal(board):
-            if pichus_count + 1 > max_pichus:
-                max_pichus_board = s
-                max_pichus = pichus_count + 1
-            fringe.append((s, pichus_count + 1))
-    return (max_pichus_board,True)
 
 # Main Function
 if __name__ == "__main__":
@@ -130,9 +277,6 @@ if __name__ == "__main__":
     # This is K, the number of agents
     k = int(sys.argv[2])
     print ("Starting from initial board:\n" + printable_board(house_map) + "\n\nLooking for solution...\n")
-
-    # start = time.process_time()
     (newboard, success) = solve(house_map, k)
-    # print(time.process_time() - start)
     print ("Here's what we found:")
     print (printable_board(newboard) if success else "None")
